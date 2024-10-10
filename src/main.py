097eee681016e72b13e8f671341987910e8830f0
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-11 01:06:01 +0200

    increase blur on gaussian image

--- src/main.py
+++ src/main.py
@@ -4,5 +4,5 @@ import image_filters as filters
 if __name__ == "__main__":
     image_data = im.load_image("assets/test/test.jpg")
     # filters.blur_optimize(image_data, 3)
-    filters.gaussian_blur_optimize(image_data, 3)
+    filters.gaussian_blur_optimize(image_data, 5)
     image_data = im.save_image(image_data, "assets/test/filtered.jpg")
