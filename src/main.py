b9f2b70d667cf5615949f717ba95d363a149ec8f
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-10 22:34:44 +0200

    reduce blur radius

--- src/main.py
+++ src/main.py
@@ -3,6 +3,5 @@ import image_filters as filters
 
 if __name__ == "__main__":
     image_data = im.load_image("assets/test/test.jpg")
-    filters.blur(image_data, 3)
-    filters.blur_optimize(image_data, 3)
+    filters.blur_optimize(image_data, 1)
     image_data = im.save_image(image_data, "assets/test/new_test.jpg")
