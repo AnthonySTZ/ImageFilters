746c1bd2f1f6c608f1c656ae27c4087cf2f25f5a
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-11 00:26:35 +0200

    create ReadMe

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ import image_filters as filters
 
 if __name__ == "__main__":
     image_data = im.load_image("assets/test/test.jpg")
-    filters.blur_optimize(image_data, 1)
+    filters.blur_optimize(image_data, 3)
     image_data = im.save_image(image_data, "assets/test/new_test.jpg")
