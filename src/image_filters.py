4ccb6b2979b6b64ba3bded5eeeefd7c920ebea05
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-10 22:27:21 +0200

    add multiprocessing for bluring

--- src/image_filters.py
+++ src/image_filters.py
@@ -1,4 +1,7 @@
 from PIL import Image
+import multiprocessing
+from multiprocessing import Process
+import timechecking as tcheck
 
 
 def get_table_pixel(image: Image) -> list:
@@ -27,6 +30,7 @@ def greyscale(image: Image) -> None:
     image.putdata(filtered_data)
 
 
+@tcheck.mesure_function_time
 def blur(image: Image, blur_radius: int) -> None:
     image_table = get_table_pixel(image)
     filtered_data = []
@@ -49,6 +53,69 @@ def blur(image: Image, blur_radius: int) -> None:
             pixel[2] = int(pixel[2] / num_pixels)
             filtered_data.append(tuple(pixel))
 
-        print(f"{y} / {height}")
+        # print(f"{y} / {height}")
 
     image.putdata(filtered_data)
+
+
+@tcheck.mesure_function_time
+def blur_cpu(image: Image, blur_radius: int) -> None:
+    image_table = get_table_pixel(image)
+
+    manager = multiprocessing.Manager()
+    returned_dict = manager.dict()
+
+    procs = []
+    nb_of_procs = multiprocessing.cpu_count()
+
+    for proc_nb in range(nb_of_procs):
+        proc = Process(
+            target=mult_proc_blur,
+            args=(proc_nb, nb_of_procs, image_table, blur_radius, returned_dict),
+        )  # instantiating without any argument
+        procs.append(proc)
+        proc.start()
+
+    for p in procs:
+        p.join()
+
+    filtered_image = []
+    # print(returned_dict)
+    for proc_nb in range(nb_of_procs):
+        filtered_image.extend(returned_dict[proc_nb])
+
+    image.putdata(filtered_image)
+
+
+def mult_proc_blur(
+    curr_process: int,
+    nb_of_processes: int,
+    base_image_table: list,
+    blur_radius: int,
+    returned_dict: dict,
+) -> None:
+    filtered_data = []
+    width, height = len(base_image_table[0]), len(base_image_table)
+    height_start = int((height / nb_of_processes) * curr_process)
+    height_end = int((height / nb_of_processes) * (curr_process + 1))
+
+    for y in range(height_start, height_end):
+        for x in range(width):
+            pixel = [0, 0, 0]
+            num_pixels = 0
+            for radius_y in range(-blur_radius, blur_radius + 1):
+                for radius_x in range(-blur_radius, blur_radius + 1):
+                    if 0 <= y + radius_y < height and 0 <= x + radius_x < width:
+                        pixel[0] += base_image_table[y + radius_y][x + radius_x][0]
+                        pixel[1] += base_image_table[y + radius_y][x + radius_x][1]
+                        pixel[2] += base_image_table[y + radius_y][x + radius_x][2]
+                        num_pixels += 1
+
+            pixel[0] = int(pixel[0] / num_pixels)
+            pixel[1] = int(pixel[1] / num_pixels)
+            pixel[2] = int(pixel[2] / num_pixels)
+            filtered_data.append(tuple(pixel))
+
+        # print(f"{y} / {height}")
+
+    returned_dict[curr_process] = filtered_data
--- src/main.py
+++ src/main.py
@@ -4,4 +4,5 @@ import image_filters as filters
 if __name__ == "__main__":
     image_data = im.load_image("assets/test/test.jpg")
     filters.blur(image_data, 3)
+    filters.blur_cpu(image_data, 3)
     image_data = im.save_image(image_data, "assets/test/new_test.jpg")
