c528e73c12026be75d66d5f8239e4938ac68ca32
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-10 22:24:55 +0200

    create mesure function execution time decorator

---
+++ timechecking.py
@@ -0,0 +1,13 @@
+import time
+
+
+def mesure_function_time(func):
+    def wrap(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+
+        print(func.__name__, end - start)
+        return result
+
+    return wrap
