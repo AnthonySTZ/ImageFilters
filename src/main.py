e818dfb3755eab738008be025925eed6afacf1e0
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-12 12:35:28 +0200

    add matrix checking

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 import image_management as im
 import image_filters as filters
+from matrix import Matrix
 
 if __name__ == "__main__":
-    image_data = im.load_image("assets/test/test.jpg")
-    # filters.blur_optimize(image_data, 3)
-    filters.gaussian_blur_optimize(image_data, 5)
-    image_data = im.save_image(image_data, "assets/test/filtered.jpg")
+    # image_data = im.load_image("assets/test/test.jpg")
+    # # filters.blur_optimize(image_data, 3)
+    # filters.gaussian_blur_optimize(image_data, 5)
+    # image_data = im.save_image(image_data, "assets/test/filtered.jpg")
+    matrix = Matrix([[0.0, 1.0, 0.0], [1.0, 4.0, 1.0], [0.0, 1.0, 0.0]])
+    matrix_2 = Matrix([[1.0, 1.0, 0.0], [2.0, 3.0, 1.0], [0.0, 1.0, 3.0]])
+    matrix_3 = Matrix([[]])
+    print(matrix)
+    print(matrix_2)
--- src/matrix.py
+++ src/matrix.py
@@ -1,6 +1,26 @@
-class Matrix3:
+class Matrix:
     def __init__(self, matrix: list[list]) -> None:
         self.matrix = matrix
+        self.check()
+
+    @property
+    def size(self) -> tuple:
+        return (len(self.matrix), len(self.matrix[0]))
+
+    def check(self) -> None:
+        if len(self.matrix) == 0:
+            raise ValueError("The matrix cannot be empty.")
+        if len(self.matrix[0]) == 0:
+            raise ValueError("All cells in the matrix cannot be empty.")
+        if not all(len(row) == len(self.matrix[0]) for row in self.matrix):
+            raise ValueError("All rows in the matrix must have the same length.")
 
     def __repr__(self) -> str:
-        return f"Matrix3:\n{self.matrix[0]}\n{self.matrix[1]}\n{self.matrix[2]}"
+        return f"Matrix of size {self.size} :\n{self.matrix[0]}\n{self.matrix[1]}\n{self.matrix[2]}"
+
+    def mult_cells(self, matrix: "Matrix") -> "Matrix":
+        result = Matrix([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        for y in range(3):
+            for x in range(3):
+                result.matrix[y][x] += self.matrix[y][x] * matrix.matrix[y][x]
+        return result
